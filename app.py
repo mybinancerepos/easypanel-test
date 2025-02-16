35ffe0da38062a6fe976d1e7f941d2bb24e2ad11
Binance GitHub <151542222+mybinancerepos@users.noreply.github.com>
2025-02-16 12:27:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-import time, requests, logging
+import time
+import logging
 
+# Set up basic logging configuration
 logging.basicConfig(level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
 
+# Counter for log print
+log_counter = 0
+
 def print_hi():
-    logging.info('This is logging')
-    print('This is print')
+    global log_counter
+    log_counter += 1
+    logging.info(f'This is logging. Log count: {log_counter}')
+    print(f'This is print. Log count: {log_counter}')
 
 if __name__ == '__main__':
     while True:
         print_hi()
         time.sleep(2)
-        logging.info("*********************************************")
